638387690c71c335ef76186e3b4cdf412a222b36
eslaweski <eslaweski@eslaweski.etc.etcusa.local>
2014-06-24 12:10:19 -0400

    Added the "License Agreement"

--- src/Dashie/Gui.java
+++ src/Dashie/Gui.java
@@ -1,10 +1,14 @@
 package Dashie;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ---------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <Eric.Slaweski@gmail.com> wrote this file. As long as you retain this notice 
+ * you can do whatever you want with this stuff. If we meet some day, and you 
+ * think this stuff is worth it, you can buy me a beer in return. Eric Slaweski
+ * ----------------------------------------------------------------------------
  */
+
 /**
  *
  * @author eslaweski
--- src/Dashie/SerialCommunicator.java
+++ src/Dashie/SerialCommunicator.java
@@ -1,5 +1,13 @@
 package Dashie;
 
+/*
+ * ---------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <Eric.Slaweski@gmail.com> wrote this file. As long as you retain this notice 
+ * you can do whatever you want with this stuff. If we meet some day, and you 
+ * think this stuff is worth it, you can buy me a beer in return. Eric Slaweski
+ * ---------------------------------------------------------------------------
+ */
 
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
